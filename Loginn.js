b3d6c08483106babf8936714bd3beac030ce0e63
Sai Kumar Sarika <135521229+sskumar2353@users.noreply.github.com>
2025-04-06 23:07:25 +0530

    Update Loginn.js

--- Loginn.js
+++ Loginn.js
@@ -17,7 +17,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
             try {
                 console.log("Attempting login...");  // Debugging Log
-                const response = await fetch("http://localhost:5000/api/login", {
+                const response = await fetch("https://ec-backend-0lvq.onrender.com/api/login", {
                     method: "POST", // Ensure this is POST, not GET
                     headers: {
                         "Content-Type": "application/json",
